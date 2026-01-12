e8f04f7653e3728944a3862ad1e274e32f4436f5
Dietrich Daroch <Dietrich@Daroch.me>
2026-01-12 19:04:04 -0300

    TS,Init: Add default grammar

---
+++ grammar.js
@@ -0,0 +1,17 @@
+/**
+ * @file Papier, A strict, simple recursive document language
+ * @author Dietrich Daroch <Dietrich@Daroch.me>
+ * @license MIT
+ */
+
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
+module.exports = grammar({
+  name: "papier",
+
+  rules: {
+    // TODO: add the actual grammar rules
+    source_file: $ => "hello"
+  }
+});
